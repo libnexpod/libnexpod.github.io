9627928315aa5efd1d9ccfa22386ee97d7c14430
Kilian Hanich <khanich.opensource@gmx.de>
2024-09-11 00:36:42 +0200

    footer
    Signed-off-by: Kilian Hanich <khanich.opensource@gmx.de>

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let footer = document.getElementById("footer");
+
+    footer.innerHTML = `
+        <p>Author: Kilian Hanich</p>
+        <p>Contact: <a href="mailto:khanich.opensource@gmx.de">khanich.opensource@gmx.de</a></p>
+    `;
+});
